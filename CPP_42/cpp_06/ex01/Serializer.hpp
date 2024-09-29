4e1c3fc3f7653a0e39328324069266b467bc7ff3
Fernanda Suominsky <82189986+Ferskys@users.noreply.github.com>
2024-09-29 01:06:00 +0000

    ex01:ok

--- CPP_42/cpp_06/ex01/Serializer.hpp
+++ CPP_42/cpp_06/ex01/Serializer.hpp
@@ -1,11 +1,12 @@
 #pragma once
 #include "Data.hpp"
+#include <stdint.h>
 
 // Use unsigned long instead of uintptr_t
 class Serializer {
 public:
-    static unsigned long serialize(Data* ptr);
-    static Data* deserialize(unsigned long raw);
+    static uintptr_t serialize(Data* ptr);
+    static Data* deserialize(uintptr_t raw);
 private:
     Serializer(); // Prevent instantiation
 };
--- CPP_42/cpp_06/ex01/main.cpp
+++ CPP_42/cpp_06/ex01/main.cpp
@@ -3,15 +3,21 @@
 #include <iostream>
 
 int main() {
+    // Cria uma instância da estrutura Data e inicializa seus membros
     Data data;
     data.value = 42;
     data.name = "Example";
     
+    // Exibe os valores originais da estrutura Data
     std::cout << "Original Data: " << data.value << ", " << data.name << std::endl;
 
+    // Serializa o ponteiro para a estrutura Data em um valor bruto (raw)
     unsigned long raw = Serializer::serialize(&data);
+
+    // Desserializa o valor bruto (raw) de volta para um ponteiro para a estrutura Data
     Data* deserializedData = Serializer::deserialize(raw);
 
+    // Exibe os valores da estrutura Data desserializada
     std::cout << "Deserialized Data: " << deserializedData->value << ", " << deserializedData->name << std::endl;
 
     return 0;
