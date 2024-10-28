ad1657d269207c819ff6c2eba4326034df04a91d
Fernanda Suominsky <82189986+Ferskys@users.noreply.github.com>
2024-10-28 00:32:08 +0000

    fix: main

--- CPP_42/cpp_07/ex01/main.cpp
+++ CPP_42/cpp_07/ex01/main.cpp
@@ -1,45 +1,43 @@
 #include <iostream>
 #include "iter.hpp"
 
+// Function to be passed to iter, which prints the value of the element
 void print(int& x) {
     std::cout << x << std::endl;
 }
 
-int main() {
-    int arr[] = {1, 2, 3, 4, 5};
-    ::iter(arr, 5, print);
-    return 0;
+// Function to be passed to iter, which doubles the value of the element
+void doubleValue(int& x) {
+    x *= 2;
 }
 
+// Function to be passed to iter, which prints the value of the element (for double type)
+void printDouble(double& x) {
+    std::cout << x << std::endl;
+}
 
-// class Awesome
-// {
-//   public:
-//     Awesome( void ) : _n( 42 ) { return; }
-//     int get( void ) const { return this->_n; }
-//   private:
-//     int _n;
-// };
+/*  Initial message explaining the purpose of the program
+    Example 1: Applying print function to an array of integers
+    Example 2: Applying doubleValue function to an array of integers and then printing the array
+    Example 3: Applying print function to an array of doubles
+    Final message indicating that the execution has finished */
+int main() {
+    std::cout << "This program demonstrates the iter function, which applies a function to each element of an array." << std::endl;
 
-// std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
-// {
-//   o << rhs.get();
-//   return o;
-// }
+    std::cout << "\nExample 1: Printing an array of integers" << std::endl;
+    int arr1[] = {1, 2, 3, 4, 5};
+    iter(arr1, 5, print);
 
-// template< typename T >
-// void print( T& x )
-// {
-//   std::cout << x << std::endl;
-//   return;
-// }
+    std::cout << "\nExample 2: Doubling the values of an array of integers and then printing" << std::endl;
+    int arr2[] = {1, 2, 3, 4, 5};
+    iter(arr2, 5, doubleValue);
+    iter(arr2, 5, print);
 
-// int main() {
-//   int tab[] = { 0, 1, 2, 3, 4 };
-//   Awesome tab2[5];
+    std::cout << "\nExample 3: Printing an array of doubles" << std::endl;
+    double arr3[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    iter(arr3, 5, printDouble);
 
-//   iter( tab, 5, print<const int> );
-//   iter( tab2, 5, print<Awesome> );
+    std::cout << "\nExecution of the iter function completed." << std::endl;
 
-//   return 0;
-// }
+    return 0;
+}
